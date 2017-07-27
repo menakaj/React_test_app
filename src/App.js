4285d8de72bdcd2c5f97fd63533bba3b9be45dd9
Menaka Jayawardena <menaka@wso2.com>
2017-07-27 23:32:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Hello from './components/comp1'
+
+class App extends Component {
+  render() {
+      return <Hello name="Menaka"/>;
+  }
+}
+
+export default App;
